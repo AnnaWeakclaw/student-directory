df99a4eb6630f4107f974d18fa014004f5e67322
Anna <annas.wieclaw@gmail.com>
2019-05-10 20:55:37 +0100

    Added grouping people by cohorts and used debugging functionality

--- exercises_directory.rb
+++ exercises_directory.rb
@@ -4,16 +4,16 @@ def input_students
   #create empty array
   students = []
   name = gets.chomp
-  
-  while !name.empty? do
+
+  while !name.empty?
     puts "Enter cohort"
-    cohort = gets.chomp
+    cohort = gets.chomp.to_sym
     puts "Enter hobby"
     hobby = gets.chomp
     cohort = "ASSIGN ME" if cohort.empty?
     hobby = "Coding" if hobby.empty?
 
-    data = {name: name, cohort: cohort, hobby: hobby}
+    data = { name: name, cohort: cohort, hobby: hobby }
     students << data
     puts "Now we have #{students.count} students"
     name = gets.chomp
@@ -29,7 +29,7 @@ end
 def print(students)
   index = 0
   while index < students.length
-    puts "#{index +1}. #{(students[index][:name]).center(30)} (#{students[index][:cohort]} cohort) HOBBY: #{students[index][:hobby]}"
+    puts "#{index + 1}. #{(students[index][:name]).center(30)} (#{students[index][:cohort]} cohort) HOBBY: #{students[index][:hobby]}"
     index += 1
   end
 end
@@ -37,7 +37,23 @@ end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
+def print_by_cohort(students)
+  cohort_names = students.uniq { |student| student[:cohort] }
+  cohort_names.each { |name|
+    puts "in cohort: #{name[:cohort]} "
+    i = 0
+    while i < students.length
+      if students[i][:cohort] == name[:cohort]
+        puts students[i]
+      end
+      i += 1
+    end
+  }
+end
+
 students = input_students
 print_header
 print(students)
 print_footer(students)
+print_by_cohort(students)
