46ecfb6ba262101f12227ff32f8e58e66594c507
Anna <annas.wieclaw@gmail.com>
2019-05-10 13:29:16 +0100

    Methods for printing header followed by students list

--- directory.rb
+++ directory.rb
@@ -13,10 +13,22 @@ students = [
  "Joffrey Baratheon",
  "Norman Bates"
 ]
-puts "The students of Villains Academy"
-puts "---------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "---------------"
 end
-#Ilustrate the difference between puts and print
-puts "Overall, we have #{students.count} great stidents"
+
+def print(names)
+  names.each do |name|
+  puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great stidents"
+end
+
+print_header
+print(students)
+print_footer(students)
