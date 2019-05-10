a5dcb6ee7857227e088e564da7eb86b99eeab322
Anna <annas.wieclaw@gmail.com>
2019-05-10 13:20:56 +0100

    Separated data(array) from the operation on it so we can easily apply varied operations to the data.

--- directory.rb
+++ directory.rb
@@ -1,16 +1,22 @@
-student_count = 11
+
+#add students to the array
+students = [
+ "Dr Hannibal Lecter",
+ "Darth Vader",
+ "Nurse Ratched",
+ "Michael Corleone",
+ "Alex DeLarge",
+ "The Wicked Witch of the West",
+ "Terminator",
+ "Freddy Krueger",
+ "The Joker",
+ "Joffrey Baratheon",
+ "Norman Bates"
+]
 puts "The students of Villains Academy"
 puts "---------------"
-puts "Dr Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
 #Ilustrate the difference between puts and print
-puts "Overall we have #{student_count} great stidents"
+puts "Overall, we have #{students.count} great stidents"
