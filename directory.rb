ff1bc72564f6b09f6ba50baf4a821bef4391d83d
Anna <annas.wieclaw@gmail.com>
2019-05-10 10:51:04 +0100

    Refactor counting the students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 puts "The students of Villains Academy"
 puts "---------------"
 puts "Dr Hannibal Lecter"
@@ -12,6 +13,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 #Ilustrate the difference between puts and print
-print "Overall we have "
-print 11
-print " great stidents"
+puts "Overall we have #{student_count} great stidents"
