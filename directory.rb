90a2dba14fb0a60d413c814ffd99b463720584e4
Anna <annas.wieclaw@gmail.com>
2019-05-12 14:35:38 +0100

    Saving students to a file and adding file to .gitignore

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ def print_menu
    # 1. print the menu and ask the user what to do
    puts "1. Input the students"
    puts "2. Show the students"
+   puts "3. Save the list to students.csv"
    puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -19,6 +20,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -58,4 +61,14 @@ end
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
+
+def save_students
+#open the file for writing
+file = File.open("students.csv", "w")
+@students.each do |student|
+  student_data = [student[:name], student[:cohort]]
+csv_line = student_data.join(",")
+file.puts csv_line
+end
+end 
 interactive_menu
